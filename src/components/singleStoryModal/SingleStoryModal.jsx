3d0703ecc364ec5a7b58844b9a03dbfba290605c
GabSalemi <salemi.gabriele94@gmail.com>
2023-06-23 17:13:19 +0200

    17.13

--- src/components/singleStoryModal/SingleStoryModal.jsx
+++ src/components/singleStoryModal/SingleStoryModal.jsx
@@ -1,8 +1,9 @@
 import "./index.css";
 import React from "react";
 import { useEffect, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+
 import { Pagination } from "swiper";
+
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -12,31 +13,31 @@ import UserHeader from "../userheader";
 
 const SingleStoryModal = ({data, openModal, closeModal, i}) => {
 
-    const [start, setStart] = useState(false);
+    const [modalState, setModalState] = useState(false);
+
     useEffect(() => {
-    setStart(i);
+    setModalState(i);
   }, [i]);
 
-  console.log(data)
  return ( 
  <div className={`${openModal ? "open" : ""} modal__stories`}>
-    <div className="ModalStories__Close" onClick={() => closeModal()}>✖</div>
+    <div className="modal__close" onClick={() => closeModal()}>✖</div>
+    <WriteMessage />
+    <UserHeader />
     <Swiper initialSlide={i} spaceBetween={50}>
         {data && data?.map((userStories) => {
-            
             <SwiperSlide key={userStories?.id} className={userStories.id}>
                 
                 <Swiper modules={[Pagination]} pagination={{type: "progressbar"}}>
                     {userStories?.stories?.map((singleStory) => (
                         <SwiperSlide key={singleStory?.id}>
                             {singleStory?.type === "image" ? (
-                                <img src="https://picsum.photos/200/300"></img>
+                                <img src={singleStory?.src}></img>
                             ) : (
                                 <video src={singleStory?.src} playsInline muted autoPlay loop></video>
                             )}
                         </SwiperSlide>
                     ))}
-
                 </Swiper>
             </SwiperSlide>
         })}
